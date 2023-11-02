15237505798b2d0fd8f663853d0c704e8e79f1b1
Dio Eracler <dioeracler1988@gmail.com>
2023-11-02 18:53:26 +0300

    create component vye footer

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import FooterElement from './Components/FooterElement.vue';
+</script>
 
 <template>
   <header>
@@ -16,13 +18,9 @@
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum et cum debitis ab dignissimos, pariatur distinctio nesciunt eaque doloribus ullam inventore. Provident assumenda alias, cum quia at veritatis reprehenderit exercitationem eaque quam ullam, cupiditate fuga illum modi autem quas, dicta fugit magni molestias eveniet natus iusto repudiandae eos. Eveniet placeat itaque, incidunt sunt quis delectus autem fugit rem nam hic tenetur sint inventore magni, beatae excepturi reprehenderit totam mollitia officia quibusdam in neque labore magnam. Possimus deserunt fugiat consectetur nemo accusamus voluptas amet ipsa magni provident totam enim culpa quibusdam neque placeat odit, est non quidem, tempore quis perferendis.
     </div>
   </main>
-  <footer>
-    <div class="logo"><a href="#">LOGO</a></div>
-    <div class="copy">
-      <div>Copyright Dio</div>
-      <div>Designed By The Provided</div>
-    </div>
-  </footer>
+  
+    <FooterElement/>
+    
 </template>
 
 <style scoped>
@@ -35,13 +33,6 @@ header {
   background-color: bisque;
 }
 
-.logo {
-  flex: 1;
-  text-align: center;
-}
-.logo>a{
-  color:wheat;
-}
 ul {
   flex: 1;
   display: flex;
@@ -63,22 +54,4 @@ h1 {
   margin: 20px;
 }
 
-footer {
-  display: flex;
-  flex-direction: row;
-  height: 100px;
-  flex: 0 0 auto;
-  align-items: center;
-  color:wheat;
-
-  /* margin-top: auto; */
-  background-color: black;
-}
-
-.copy{
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  align-items: center
-}
 </style>
---
+++ src/Components/FooterElement.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    <div class="logo"><a href="#">LOGO</a></div>
+    <div class="copy">
+      <div>Copyright Dio</div>
+      <div>Designed By The Provided</div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  display: flex;
+  flex-direction: row;
+  height: 100px;
+  flex: 0 0 auto;
+  align-items: center;
+  color:wheat;
+
+  /* margin-top: auto; */
+  background-color: black;
+}
+
+.copy{
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center
+}
+</style>
