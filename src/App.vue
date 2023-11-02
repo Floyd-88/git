60932ffa29fd7c00f71f97a373402d17c6ef52ae
Dio Eracler <dioeracler1988@gmail.com>
2023-11-02 14:28:22 +0300

    title

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
     </ul>
   </header>
   <main>
+    <h1>Заголовок</h1>
     <div>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia dolorum et cum debitis ab dignissimos, pariatur distinctio nesciunt eaque doloribus ullam inventore. Provident assumenda alias, cum quia at veritatis reprehenderit exercitationem eaque quam ullam, cupiditate fuga illum modi autem quas, dicta fugit magni molestias eveniet natus iusto repudiandae eos. Eveniet placeat itaque, incidunt sunt quis delectus autem fugit rem nam hic tenetur sint inventore magni, beatae excepturi reprehenderit totam mollitia officia quibusdam in neque labore magnam. Possimus deserunt fugiat consectetur nemo accusamus voluptas amet ipsa magni provident totam enim culpa quibusdam neque placeat odit, est non quidem, tempore quis perferendis.
     </div>
