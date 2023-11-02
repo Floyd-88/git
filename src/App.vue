3f1f237cda6f857febaddef7a81827cd1e3938b9
Dio Eracler <dioeracler1988@gmail.com>
2023-11-02 12:59:55 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,45 @@
 </script>
 
 <template>
-    <header></header>
+    <header>
+      <div class="logo">LOGO</div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Content</li>
+        <li>Contacts</li>
+      </ul>
+    </header>
     <main></main>
     <footer></footer>
 </template>
 
 <style scoped>
 header {
-  height: 150px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100px;
   flex: 0 0 auto;
   background-color: bisque;
 }
+
+.logo {
+  flex: 1;
+  text-align: center;
+}
+ul{
+  flex: 1;
+  display: flex;
+  list-style-type: none;
+  font-size: 20px;
+  font-weight: 600;
+  
+}
+ul>li {
+  padding-right: 20px;
+}
+
 main {
   flex: 1 0 auto;
   background-color: darkcyan;
