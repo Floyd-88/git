0cd9e39fc60903dc4a7b2431ce22f3a777c89434
Dio Eracler <dioeracler1988@gmail.com>
2023-11-04 15:03:18 +0300

    ref article

--- src/Components/MainElement.vue
+++ src/Components/MainElement.vue
@@ -4,17 +4,36 @@
     <div class="main_wrapper">
       <div class="content">
         <div>gdfbfd fgsdgfd</div>
-        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, temporibus?</div>
+        <div ref="articleRef">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, temporibus?
+        </div>
       </div>
-      <AsideElement/>
-     
+      <AsideElement />
     </div>
   </main>
 </template>
 <script setup>
-import AsideElement from '@/Components/AsideElement.vue';
+import AsideElement from '@/Components/AsideElement.vue'
+import { watchEffect, ref } from 'vue'
 
+let search = ref('dd')
+let articleRef = ref(null)
+let article = ref('')
 
+watchEffect(
+  () => {
+    article.value = articleRef.value.innerHTML
+  },
+  {
+    flush: 'post'
+  }
+)
+
+function getSearch(search) {
+  console.log(search)
+  return search
+}
+getSearch(search)
 </script>
 <style scoped>
 main {
@@ -28,22 +47,19 @@ h1 {
   margin: 20px;
 }
 .main_wrapper {
-    display: flex;
-    flex-direction: row;
-    height: 80%;
-
+  display: flex;
+  flex-direction: row;
+  height: 80%;
 }
 
 .content {
-    flex: 3;
-    margin-right: 10px;
-    padding: 5px;
-    border: 1px solid whitesmoke;
-    border-radius: 10px;
-    background-color: whitesmoke;
-    font-size: 17px;
-    line-height: 1.5rem;
+  flex: 3;
+  margin-right: 10px;
+  padding: 5px;
+  border: 1px solid whitesmoke;
+  border-radius: 10px;
+  background-color: whitesmoke;
+  font-size: 17px;
+  line-height: 1.5rem;
 }
-
-
 </style>
